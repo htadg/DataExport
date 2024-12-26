7dee71d441c999bc1fe21389bce1da371a68505e
Hiten Sharma <sharma.hiten3@gmail.com>
2024-12-26 11:46:45 +0530

    fixed database details updated docker-compose file added init script for postgresql

---
+++ asset/db-init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE cars (
+    brand VARCHAR(255),
+    model VARCHAR(255),
+    year INT
+);
+
+
+INSERT INTO cars (brand, model, year)
+VALUES
+    ('Ford', 'Mustang', 1964),
+    ('Volvo', 'p1800', 1968),
+    ('BMW', 'M1', 1978),
+    ('Toyota', 'Celica', 1975);
